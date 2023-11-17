f1c6c5eaaf2b9393df45041c4b84b289c1c62d2c
kakuta0915 <kakuta19960915@gmail.com>
2023-11-17 13:11:33 +0900

    🔧 update: babel-loaderの設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   entry: "./dist/main.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js&/,
+        use: "babel-loader",
+      },
+    ],
+  },
 };
